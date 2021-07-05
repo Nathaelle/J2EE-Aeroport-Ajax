0e191c106f1e7472cc585922a30523185b5fa843
Nathaelle <scriptales.clients@gmail.com>
2021-07-05 16:21:11 +0200

    Démo ObjectMapper Jackson

--- src/main/java/servlets/AeroportDatas.java
+++ src/main/java/servlets/AeroportDatas.java
@@ -1,6 +1,11 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -28,18 +33,25 @@ public class AeroportDatas extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		Piste p = new Piste(2, 1500, 6, "N", 1);
+		HashMap<String,Piste> al = new HashMap<String,Piste>();
+		Piste p1 = new Piste(2, 1500, 6, "N", 1);
+		Piste p2 = new Piste(3, 2500, 10, "S", 1);
+		Piste p3 = new Piste(4, 600, 6, "E", 1);
+		Piste p4 = new Piste(5, 1500, 6, "O", 1);
+		al.put("Piste 1", p1);
+		al.put("Piste 2", p2);
+		al.put("Piste 3", p3);
+		al.put("Piste 4", p4);
+		
+		
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+		mapper.writeValue(response.getWriter(), al);
+		System.out.println(response);
 		
 		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/plain");
-		response.getWriter().append("Communication réussie !");
+		response.setContentType("text/json");
 	}
 
 	/**
