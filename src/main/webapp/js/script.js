647b5cd8ab7b000be376165a2a5cc55baf7218df
Nathaelle <scriptales.clients@gmail.com>
2021-07-07 13:25:39 +0200

    Affichage des aéroports en JS

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -19,17 +19,35 @@ function getRequest() {
 
 var request = getRequest();
 console.log(request);
+request.overrideMimeType("text/json");
 
-request.open("GET", "aeroport");
+// 1. Première étape : Initialisation de la requête
+request.open("GET", "aeroport?data=airports");
+// 2. Deuxième étape : Ajout des ent-têtes HTTP
 request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// 3. Troisième et dernière étape : Envoi de la requête
 request.send();
 
 request.onreadystatechange = () => {
 	
 	if(request.readyState == 4 && request.status == 200) {
-		document.getElementById("content").innerHTML = request.responseText;
-		
+		printAirports(request.responseText);
 	}
 };
+
+
+function printAirports(jsonString) {
+	
+	var json = JSON.parse(jsonString);
+	
+	var html = "";
+	for(let aeroport of json) {
+		html += "<h2>" + aeroport.nom + "</h2>";
+		html += "<h3>Vols du jour</h3>";
+	}
+	
+	document.getElementById("airports").innerHTML = html;
+}
+
 
 
